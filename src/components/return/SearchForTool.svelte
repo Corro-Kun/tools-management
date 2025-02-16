a9132fd13d21285908fe0c7867b372d4682d101f
Corro kun <networpava@gmail.com>
2025-02-15 21:24:52 -0500

    The filter to return the tools was added

---
+++ src/components/return/SearchForTool.svelte
@@ -0,0 +1,28 @@
+<script>
+    import Input from "@/shared/components/Input.svelte";
+    import Button from "@/shared/components/Button.svelte";
+</script>
+
+<form>
+    <Input required placeholder="ID del equipo" />
+    <div>
+        <Button type="submit" >Devolver</Button>
+    </div>
+</form>
+
+<style>
+    form{
+        display: flex;
+        width: 400px;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 15px;
+    }
+    div{
+        width: 200px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
